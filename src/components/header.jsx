099ca01f4e6774679bb2d5550be9887616dc331c
frako23 <frako23@gmail.com>
2023-10-22 18:59:04 -0400

    hero completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import header from "../assets/imgs/header.jpg";
+import header from "../assets/imgs/hero2.png";
 import "../styles/home.css";
 
 export function Headers() {
@@ -8,18 +8,18 @@ export function Headers() {
       <div className="container main__header-container">
         <div className="main__header-left">
           <h4>
-            <span>Fitness</span> is my PASSION
+            Fitness is my <span>PASSION</span>
           </h4>
           <h1>Brenda Orozco Personal Trainer</h1>
-          <h5>
+          <p>
             Improve your performance, enhance you skills, may goal is to get you
             to the next level straightforward
-          </h5>
+          </p>
           <Link to="/plans" className="btn lg">
             Get Started
           </Link>
         </div>
-        <div className="main__header-rigth">
+        <div className="main__header-right">
           <div className="main__header-circle"></div>
           <div className="main__header-image">
             <img src={header} alt="Main Header Image" />
